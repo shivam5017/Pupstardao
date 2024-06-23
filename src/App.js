fe4a6e159bfac4d43c9e28d405d625212f75dd75
shivam5017 <shivammalik962@gmail.com>
2024-06-23 19:57:48 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 
+import Navbar from "./Components/Navbar/Navbar";
+
+
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <h1>Pupstardao</h1>
+    <div className="main-div">
+      <Navbar />
+       
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Navbar.css"
+import { IoMdSettings } from "react-icons/io"
+import Logo from "../../Images/logo.jpeg"
+
+const Navbar = () => {
+
+  return (
+    <div className='nav-div'>
+       <img src={Logo} className='logo'/>
+        <IoMdSettings size={24} className='settings-icon'/>
+    </div>
+  )
+}
+
+export default Navbar
